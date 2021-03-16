2189563ecdc5ff6a04805251e0ead56ab7595668
KevinC911 <00157320@uca.edu.sv>
2021-03-16 08:50:54 -0600

    Adding the example program

---
+++ Lab01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 0;
+            var b = 0;
+
+            Console.WriteLine("Enter a value for a");
+            a = Int32.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Enter a value for b");
+            b = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Adding {a} + {b} equals to {AddNumbers(a,b)}");
+        }
+
+        public static int AddNumbers(int a, int b)
+        {
+            return a+b;
+        }
+    }
+}
